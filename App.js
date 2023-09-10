ceff22f22b1a9ad4c1b432161686208a083a40a4
NathanLombardelli <nathanlombardelli@hotmail.com>
2023-09-10 15:44:05 +0200

    git init

---
+++ App.js
@@ -0,0 +1,116 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import { useState } from 'react';
+
+
+import ImageViewer from "./components/ImageViewer";
+const PlaceHolderImage = require('./assets/images/background-image.png');
+
+import Button from "./components/Button";
+import IconButton from "./components/IconButton";
+import CircleButton from "./components/CircleButton";
+import EmojiPicker from "./components/EmojiPicker";
+import EmojiList from './components/EmojiList';
+import EmojiSticker from "./components/EmojiSticker";
+
+import * as ImagePicker from 'expo-image-picker';
+
+
+export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [showAppOptions, setShowAppOptions] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [pickedEmoji, setPickedEmoji] = useState(null);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+
+  const onReset = () => {
+    setShowAppOptions(false);
+  };
+
+  const onAddSticker = () => {
+    setIsModalVisible(true);
+  };
+
+  const onSaveImageAsync = async () => {
+    // we will implement this later
+  };
+  const onModalClose = () => {
+    setIsModalVisible(false);
+  };
+
+
+  return (
+    <View style={styles.container}>
+
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={PlaceHolderImage} selectedImage={selectedImage} />
+        {pickedEmoji !== null ? <EmojiSticker imageSize={40} stickerSource={pickedEmoji} /> : null}
+      </View>
+      {showAppOptions ? (
+          <View style={styles.optionsContainer}>
+            <View style={styles.optionsRow}>
+              <IconButton icon="refresh" label="Reset" onPress={onReset} />
+              <CircleButton onPress={onAddSticker} />
+              <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
+            </View>
+          </View>
+      ) : (
+          <View style={styles.footerContainer}>
+            <Button theme="primary" label="Choose a photo" onPress={pickImageAsync}/>
+            <Button label="Use this photo" onPress={()=> setShowAppOptions(true)}/>
+          </View>
+      )}
+
+      <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+        <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
+      </EmojiPicker>
+
+      <StatusBar style="auto" />
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image:{
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
+  optionsContainer: {
+    position: 'absolute',
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+
+});
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# React_Native_learning
+# React Native learning
+
+## Date(s)
+
+09/09/2023 -> ?
+
+## Description
+
+Apprentisage de React Native via le tuto : https://docs.expo.dev/tutorial/introduction/
+
+## Team
+
+[Nathan Lombardelli](https://github.com/NathanLombardelli) <br>
+
+[//]: # (## Link)
+[//]: # ()
+[//]: # (liens vers la [page]&#40;url&#41;)
